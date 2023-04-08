9bd233deabf20dcfc57fff2eff0518ed54c4e69c
DikshakAdhikari <ddikshakk@gmail.com>
2023-04-08 12:41:56 +0530

    second

--- client/src/utils/context.js
+++ client/src/utils/context.js
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 export const Context= createContext();
 
-const AppContext= ({ children }) => { //this children that we passed as a prop are the all components inside AppContext in App.js
+const AppContext= ({ children }) => { //1. this children that we passed as a prop are the all components inside AppContext in App.js
     return (
         <Context.Provider>
             {children} 
