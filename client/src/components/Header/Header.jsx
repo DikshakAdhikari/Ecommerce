a795c63a22aa6a0eb3608430b15e23f8b061277b
DikshakAdhikari <ddikshakk@gmail.com>
2023-04-08 16:30:03 +0530

    second_3

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,37 @@
+import {useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+    
+    import Search from "./Search/Search";
+    import Cart from "../Cart/Cart";
+    import { Context } from "../../utils/context";
+
+    import{ TbSearch } from "react-icons/tb";
+    import { CgShoppingCart } from "react-icons/cg"; 
+    import { AiOutlineHeart } from "react-icons/ai" ;
+    
+
 import "./Header.scss";
 const Header = () => {
-    return <div>Header</div>;
+    return (
+     <header className="main-header">
+        <div className="header-content">
+            <ul className="left">
+                <li>Home</li>
+                <li>About</li>
+                <li>Categories</li>
+            </ul>
+            <div className="center">SOUNDSTORE</div>
+            <div className="right">
+                <TbSearch />
+                <AiOutlineHeart />
+                <span className="cart-icon">
+                    <CgShoppingCart />
+                    <span>5</span>
+                </span>
+            </div>
+        </div>
+    </header>
+    );
 };
 
 export default Header;
--- client/src/utils/context.js
+++ client/src/utils/context.js
@@ -1,7 +1,7 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const Context= createContext();
-
+//2. In context.js we'll write all logic of our application ex- cart logic like set items in cart or update items in cart. It means we'll put all global states logic that we need in multiple places we'll write in context.js and will use those in different other components.
 const AppContext= ({ children }) => { //1. this children that we passed as a prop are the all components inside AppContext in App.js
     return (
         <Context.Provider>
