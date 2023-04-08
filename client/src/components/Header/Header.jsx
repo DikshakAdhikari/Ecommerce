1de0aadc7b9dc9c67db963185ffae8f03aabcc13
DikshakAdhikari <ddikshakk@gmail.com>
2023-04-09 01:19:23 +0530

    third_stickyHeader

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -12,8 +12,22 @@ import { useNavigate } from "react-router-dom";
 
 import "./Header.scss";
 const Header = () => {
+    const [scrolled, setScrolled]= useState(false);
+
+    const handleScroll= () => { 
+        const offset= window.scrollY //9. We want our header to get stickey after some point, so we want that as our scroll amount i.e. offset goes over 200px height then we want our header should be stickey. So for that we do "setScrolled" state to be true, else "setScrolled" state remains false.
+        if(offset > 200){
+            setScrolled(true);
+        }else{
+            setScrolled(false);
+        }
+    };
+
+    useEffect(()=> { 
+        window.addEventListener("scroll",handleScroll)  //8.It means our even would be scroll and on scrolling we want handleScroll method to run
+    }, [])
     return (
-     <header className="main-header">
+     <header className={`main-header ${scrolled ? 'sticky-header': ''}`}> {/*10. so by using our state, whenever our "scrolled" state is true we add our 'stickey-header' class, else false then do nothing */}
         <div className="header-content">
             <ul className="left">
                 <li>Home</li>
