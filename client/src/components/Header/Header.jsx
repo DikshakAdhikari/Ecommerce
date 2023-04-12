f7821fe6843eddfaf8c3b85ad46246a60639b027
DikshakAdhikari <ddikshakk@gmail.com>
2023-04-12 16:58:02 +0530

    eight BEFORE STRAPI

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -13,6 +13,8 @@ import { useNavigate } from "react-router-dom";
 import "./Header.scss";
 const Header = () => {
     const [scrolled, setScrolled]= useState(false);
+    const [showCart, setShowCart]= useState(false);
+    const [showSearch, setShowSearch]= useState(false);
 
     const handleScroll= () => { 
         const offset= window.scrollY //9. We want our header to get stickey after some point, so we want that as our scroll amount i.e. offset goes over 200px height then we want our header should be stickey. So for that we do "setScrolled" state to be true, else "setScrolled" state remains false.
@@ -27,6 +29,9 @@ const Header = () => {
         window.addEventListener("scroll",handleScroll)  //8.It means our even would be scroll and on scrolling we want handleScroll method to run
     }, [])
     return (
+        <>
+
+        
      <header className={`main-header ${scrolled ? 'sticky-header': ''}`}> {/*10. so by using our state, whenever our "scrolled" state is true we add our 'stickey-header' class, else false then do nothing */}
         <div className="header-content">
             <ul className="left">
@@ -36,15 +41,18 @@ const Header = () => {
             </ul>
             <div className="center">SOUNDSTORE</div>
             <div className="right">
-                <TbSearch />
+                <TbSearch onClick={() => setShowSearch(true)} />
                 <AiOutlineHeart />
-                <span className="cart-icon">
+                <span className="cart-icon" onClick={() => setShowCart(true)}>
                     <CgShoppingCart />
                     <span>5</span>
                 </span>
             </div>
         </div>
     </header>
+   {showCart && <Cart setShowCart={setShowCart} />}
+   {showSearch && <Search setShowSearch={setShowSearch} />}
+    </>
     );
 };
 
--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,6 +1,22 @@
 import "./Products.scss";
-const Products = () => {
-    return <div>Products</div>;
+import Product from "./Product/Product"
+const Products = ({innerPage , headingText}) => {
+    return (
+         <div className="products-container">
+        {!innerPage && <div className="sec-heading">{headingText}</div>} {/*20. It means wherever innerPage is false there this peice of line will render*/}
+        <div className="products">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            
+        </div>
+        </div>
+    );
 };
 
 export default Products;
